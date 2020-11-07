50ebf4a0dc50898b447151b32902b83241ef5b4b
UlianaKov-hab <a_k_ula@ukr.net>
2020-11-07 16:11:00 +0200

    Add struct Worker and Department

--- Project8/Main.cpp
+++ Project8/Main.cpp
@@ -1,18 +1,20 @@
 #include "Department.h"
+#include<time.h>
 using namespace std;
 void main()
 {
-	srand(time(0));
+	/*srand(time(0));*/
 	Department departments[4];
-	int NumberDepartment[4]{ 1, 2, 3, 4 };
-	//NumberDepartment = 1 + rand() % 4;
-	for (int i = 0; i < 3; i++)
-	{
-		departments[i].CreateDepartment(NumberDepartment[i])
-
-	}
+	int NumberDepartment[4] = { 1, 2, 3, 4 };
 	
-
-
-
+		for (int i = 0; i < 4; i++)
+		{
+			departments[i].CreateDepartment();
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			cout << "Department number # ";
+			cout << NumberDepartment[i] << endl;
+			departments[i].PrintDepartment();
+		}
 }
