5721316ac7f91e8686059f5b549fc803e3e810fe
UlianaKov-hab <a_k_ula@ukr.net>
2020-11-07 18:08:29 +0200

    Revert "Aligned deys and hours"
    This reverts commit 1da219e138e1346b11b62fc5bbeb13d9298d2c3f.

--- Project8/Worker.cpp
+++ Project8/Worker.cpp
@@ -13,12 +13,10 @@ void Worker::CreateWorker()
 	LastName = ln[b];
 	Position = pos[c];
 	PersonalNumber = 4789 + rand() % 5639;
-	for (int i=0; i < 12; i++)
+	for (int j=0; j < 12; j++)
 	{
-		
-		NumberDaysNoWorked[i] = rand() % 22;
-		NumberHoursWorked[i] = ((22 - NumberDaysNoWorked[i]) * 8);
-		//NumberHoursWorked[i] = 100 + rand() % 160;
+		NumberHoursWorked[j] = 100 + rand() % 164;
+		NumberDaysWorked[j] = 1+rand() % 22;
 	}
 	
 }
@@ -29,16 +27,16 @@ void Worker::PrintWorker()
 	cout << "Last Name: " << LastName << endl;
 	cout << "Position: " << Position << endl;
 	cout << "Personal number: " << PersonalNumber << endl;
-	cout << "Hours worked per month:   ";
+	cout << "Hours worked per month: ";
 	for (int i=0; i < 12; i++)
 	{
-		cout << setw(3)<<NumberHoursWorked[i] << " ";
+		cout << NumberHoursWorked[i] << " ";
 	}
 	cout << endl;
-	cout << "No Worked days in a month: ";
+	cout << "Worked days in a month: ";
 	for (int i=0; i < 12; i++)
 	{
-		cout << setw(2) << NumberDaysNoWorked[i] << "  ";
+		cout << NumberDaysWorked[i] << " ";
 	}
 	cout << endl;
 
