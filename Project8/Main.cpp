67fc5fec5d4657a1b9da5f9aff6069815caff8a3
UlianaKov-hab <a_k_ula@ukr.net>
2020-11-07 19:14:58 +0200

    All OK

--- Project8/Main.cpp
+++ Project8/Main.cpp
@@ -1,18 +1,20 @@
 #include "Department.h"
+#include<time.h>
 using namespace std;
 void main()
 {
 	srand(time(0));
-	Department departments[4];
-	int NumberDepartment[4]{ 1, 2, 3, 4 };
-	//NumberDepartment = 1 + rand() % 4;
-	for (int i = 0; i < 3; i++)
+	
+	Department* departments = new Department[5];
+		
+	for (int i = 0; i < 5; i++)
 	{
-		departments[i].CreateDepartment(NumberDepartment[i])
-
+		departments[i].CreateDepartment();
+		cout << "Department number # ";
+		cout << i+1 << endl;
+		departments[i].PrintDepartment();
 	}
 	
-
-
+	
 
 }
--- Project8/Worker.cpp
+++ Project8/Worker.cpp
@@ -13,10 +13,10 @@ void Worker::CreateWorker()
 	LastName = ln[b];
 	Position = pos[c];
 	PersonalNumber = 4789 + rand() % 5639;
-	for (int i; i < 12; i++)
+	for (int i=0; i < 12; i++)
 	{
-		NumberHoursWorked[i] = 10 + rand() % 164;
-		NumberDaysWorked[i] = rand() % 22;
+		NumberDaysNoWorked[i] = 1 + rand() % 22;
+		NumberHoursWorked[i] = (22 - NumberDaysNoWorked[i]) * 8;
 	}
 	
 }
@@ -27,16 +27,16 @@ void Worker::PrintWorker()
 	cout << "Last Name: " << LastName << endl;
 	cout << "Position: " << Position << endl;
 	cout << "Personal number: " << PersonalNumber << endl;
-	cout << " Hours worked per month: ";
-	for (int i; i < 12; i++)
+	cout << "Hours worked per month:    ";
+	for (int i=0; i < 12; i++)
 	{
-		cout << NumberHoursWorked[i] << " ";
+		cout << setw(3)<<NumberHoursWorked[i] << " ";
 	}
 	cout << endl;
-	cout << " Worked days in a month: ";
-	for (int i; i < 12; i++)
+	cout << "No Worked days in a month: ";
+	for (int i=0; i < 12; i++)
 	{
-		cout << NumberDaysWorked[i] << " ";
+		cout << setw(3) << NumberDaysNoWorked[i] << " ";
 	}
 	cout << endl;
 
